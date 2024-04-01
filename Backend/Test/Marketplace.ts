66bcdf33f42ea19028dd37d35caae311a23e04ff
Harry Doan <doankhanhdong3105@gmail.com>
2024-04-01 21:39:21 +0700

    Add new test cases

--- Backend/Test/Marketplace.ts
+++ Backend/Test/Marketplace.ts
@@ -2,6 +2,10 @@ import { ethers } from "hardhat";
 import { expect } from "chai";
 import { describe } from "mocha";
 import { DTC, DTC__factory } from "../typechain-types";
+import { Marketplace } from "../typechain-types/Contracts/Marketplace";
+
+const toWei = (num: number) => ethers.parseEther(num.toString());
+const fromWei = (num: number) => ethers.formatEther(num);
 
 describe("NFTMarketplace", () => {
   let DTC: DTC__factory;
@@ -23,16 +27,60 @@ describe("NFTMarketplace", () => {
     marketplace = await Marketplace.deploy(feePercent);
   });
 
-  it("Should track each minted NFT", async function () {
-    // addr1 mints an nft
-    await dtc.connect(addr1).mintNewToken(addr1, tokenURI);
-    expect(await dtc._nextTokenId()).to.equal(1);
-    expect(await dtc.balanceOf(addr1.address)).to.equal(1);
-    expect(await dtc.tokenURI(1)).to.equal(tokenURI);
-    // addr2 mints an dtc
-    await dtc.connect(addr2).mintNewToken(addr2, tokenURI);
-    expect(await dtc._nextTokenId()).to.equal(2);
-    expect(await dtc.balanceOf(addr2.address)).to.equal(1);
-    expect(await dtc.tokenURI(2)).to.equal(tokenURI);
+  describe("Deployment", async () => {
+    const tokenName: string = "Dustin Token";
+    const tokenSymbol: string = "DTCv2";
+
+    it("Should track name and symbol of the nft collection", async function () {
+      expect(await dtc.name()).to.equal(tokenName);
+      expect(await dtc.symbol()).to.equal(tokenSymbol);
+    });
   });
+
+  describe("Minting NFTs", () => {
+    it("Should track each minted NFT", async function () {
+      await dtc.connect(addr1).mintNewToken(addr1, tokenURI);
+      expect(await dtc._nextTokenId()).to.equal(1);
+      expect(await dtc.balanceOf(addr1.address)).to.equal(1);
+      expect(await dtc.tokenURI(1)).to.equal(tokenURI);
+
+      await dtc.connect(addr2).mintNewToken(addr2, tokenURI);
+      expect(await dtc._nextTokenId()).to.equal(2);
+      expect(await dtc.balanceOf(addr2.address)).to.equal(1);
+      expect(await dtc.tokenURI(2)).to.equal(tokenURI);
+    });
+
+    it("Should track each minted the second NFT", async function () {
+      await dtc.connect(addr1).mintNewToken(addr1, tokenURI);
+      expect(await dtc._nextTokenId()).to.equal(1);
+      expect(await dtc.balanceOf(addr1.address)).to.equal(1);
+      expect(await dtc.tokenURI(1)).to.equal(tokenURI);
+
+      await dtc.connect(addr2).mintNewToken(addr2, tokenURI);
+      expect(await dtc._nextTokenId()).to.equal(2);
+      expect(await dtc.balanceOf(addr2.address)).to.equal(1);
+      expect(await dtc.tokenURI(2)).to.equal(tokenURI);
+    });
+  });
+
+  // describe("Making marketplace items", () => {
+  //   let price = 1;
+  //   let tokenID = 1;
+  //   let result;
+  //   let marketplaceAddress = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266";
+  //   beforeEach(async () => {
+  //     await dtc.connect(addr1).mintNewToken(addr1, tokenURI);
+  //     await dtc.connect(addr1).setApprovalForAll(marketplaceAddress, true);
+  //   });
+
+  //   it("Should track newly created item, transfer NFT from seller to marketplace and emit Offered event", async () => {
+  //     await expect(
+  //       marketplace
+  //         .connect(addr1)
+  //         .makeNewItem(dtc.address, tokenID, toWei(price))
+  //     )
+  //       .to.emit(marketplace, "Offered")
+  //       .withArgs(1, dtc.address, 1, toWei(price), addr1.address);
+  //   });
+  // });
 });
